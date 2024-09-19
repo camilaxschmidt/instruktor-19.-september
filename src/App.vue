ecd2191b5cc0ae5cfe491b6a04da5d8d12c45372
camilaxschmidt <161475210+camilaxschmidt@users.noreply.github.com>
2024-09-19 16:39:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const aNumber = ref(3);
+
+    function test(event) {
+      console.log('test');
+    }
+
+    return {
+      test,
+      aNumber,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>@click="test" style = "height": 100%; width: 100%; background</div>
+</template>
+
+<style></style>
